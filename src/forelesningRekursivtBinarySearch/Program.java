4c4ba33a6cb5445831b27dbd6a9db85c0057e6fa
fredrikvogt <s315714@oslomet.no>
2020-11-15 13:16:53 +0100

    Eksamen

--- src/forelesningRekursivtBinarySearch/Program.java
+++ src/forelesningRekursivtBinarySearch/Program.java
@@ -5,119 +5,136 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Program {
+    public static void main(String[] args) {
+ /*       int[] a = null;
+        int[] c = null;
 
-        public static void bytt(int[] a, int i, int j)
-        {
-            int temp = a[i]; a[i] = a[j]; a[j] = temp;
+      //  Tabell.maksOppgave3(a,-1,10);
+        Tabell.maksOppgave3(a,0,0);
+
+        int n = 100_000;        // tabellstørrelse
+
+        int antall = 2_000;     // antall gjentagelser
+
+        long tid = 0;           // for tidsmåling
+
+        int a[] = randPerm(n);  // en permutasjon av 1, . .  n
+
+        tid = System.currentTimeMillis();  // leser av klokken
+        for (int i = 0; i < antall; i++) kostnader(a);
+        tid = System.currentTimeMillis() - tid;  // medgått tid
+        System.out.println("Faste kostnader: " + tid + " millisek");
+
+        tid = System.currentTimeMillis();  // leser av klokken
+        for (int i = 0; i < antall; i++) maks1(a);
+        tid = System.currentTimeMillis() - tid;  // medgått tid
+        System.out.println("Maks1-metoden: " + tid + " millisek");
+
+        tid = System.currentTimeMillis();  // leser av klokken
+        for (int i = 0; i < antall; i++) maks2(a);
+        tid = System.currentTimeMillis() - tid;  // medgått tid
+        System.out.println("Maks2-metoden: " + tid + " millisek");
+
+        tid = System.currentTimeMillis();  // leser av klokken
+        for (int i = 0; i < antall; i++) maks3(a);
+        tid = System.currentTimeMillis() - tid;  // medgått tid
+        System.out.println("Maks3-metoden: " + tid + " millisek");
+
+ */
+
+        int[] a = Tabell.randPerm(20); // tilfeldig permutasjon av 1 . . 20
+        int[] b = Tabell.nestMaks(a);  // metoden returnerer en tabell
+
+        int m = b[0], nm = b[1];       // m for maks, nm for nestmaks
+
+        Tabell.skrivln(a);        // se Oppgave 5 i Avsnitt 1.2.2
+        System.out.print("Størst(" + a[m] + ") har posisjon " + m);
+        System.out.println(", nest størst(" + a[nm] + ") har posisjon " + nm);
+
+
+    }
+
+
+    public static void bytt(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static int[] randPerm(int n) {
+        int[] a = new int[n]; // fyller tabellen med 1, 2, . . , n
+        for (int i = 0; i < n; i++) a[i] = i + 1;
+
+        Random r = new Random();  // hentes fra java.util
+
+        for (int k = n - 1; k > 0; k--) {
+            int i = r.nextInt(k + 1);  // tilfeldig tall fra [0,k]
+            bytt(a, k, i);
         }
 
-        public static int[] randPerm(int n)
+        return a; // tabellen med permutasjonen returneres
+
+    } // randPerm
+
+    public static int kostnader(int[] a) // legges i class Program
+    {
+        int m = 0;
+        for (int i = 1; i < a.length; i++) {
+        } // en tom blokk
+        return m;
+    }
+
+    public static int maks1(int[] a)  // a er en heltallstabell
+    {
+        if (a.length < 1) throw new IllegalArgumentException("a er tom");
+
+        int m = 0;  // indeks til største verdi
+
+        for (int i = 1; i < a.length; i++) // obs: starter med i = 1
         {
-            int[] a = new int[n]; // fyller tabellen med 1, 2, . . , n
-            for (int i = 0; i < n; i++) a[i] = i+1;
-
-            Random r = new Random();  // hentes fra java.util
-
-            for (int k = n-1; k > 0; k--)
-            {
-                int i = r.nextInt(k+1);  // tilfeldig tall fra [0,k]
-                bytt(a,k,i);
-            }
-
-            return a; // tabellen med permutasjonen returneres
-
-        } // randPerm
-
-        public static int kostnader(int[] a) // legges i class Program
-        {
-            int m = 0;
-            for (int i = 1; i < a.length; i++) {} // en tom blokk
-            return m;
+            if (a[i] > a[m]) m = i;  // indeksen oppdateres
         }
 
-        public static int maks1(int[] a)  // a er en heltallstabell
-        {
-            if (a.length < 1) throw new IllegalArgumentException("a er tom");
+        return m;  // returnerer indeksen/posisjonen til største verdi
 
-            int m = 0;  // indeks til største verdi
+    } // maks
 
-            for (int i = 1; i < a.length; i++) // obs: starter med i = 1
-            {
-                if (a[i] > a[m]) m = i;  // indeksen oppdateres
-            }
+    public static int maks2(int[] a)   // versjon 2 av maks-metoden
+    {
+        int m = 0;             // indeks til største verdi
+        int maksverdi = a[0];  // største verdi
 
-            return m;  // returnerer indeksen/posisjonen til største verdi
-
-        } // maks
-
-        public static int maks2(int[] a)   // versjon 2 av maks-metoden
-        {
-            int m = 0;             // indeks til største verdi
-            int maksverdi = a[0];  // største verdi
-
-            for (int i = 1; i < a.length; i++) if (a[i] > maksverdi)
-            {
+        for (int i = 1; i < a.length; i++)
+            if (a[i] > maksverdi) {
                 maksverdi = a[i];  // største verdi oppdateres
                 m = i;             // indeks til største verdi oppdaters
             }
-            return m;  // returnerer indeks/posisjonen til største verdi
+        return m;  // returnerer indeks/posisjonen til største verdi
 
-        } // maks
+    } // maks
 
-        public static int maks3(int[] a)  // versjon 3 av maks-metoden
-        {
-            int sist = a.length - 1;       // siste posisjon i tabellen
-            int m = 0;                     // indeks til største verdi
-            int maksverdi = a[0];          // største verdi
-            int temp = a[sist];            // tar vare på siste verdi
-            a[sist] = 0x7fffffff;          // legger tallet 2147483647 sist
+    public static int maks3(int[] a)  // versjon 3 av maks-metoden
+    {
+        int sist = a.length - 1;       // siste posisjon i tabellen
+        int m = 0;                     // indeks til største verdi
+        int maksverdi = a[0];          // største verdi
+        int temp = a[sist];            // tar vare på siste verdi
+        a[sist] = 0x7fffffff;          // legger tallet 2147483647 sist
 
-            for (int i = 1; ; i++)         // i starter med 1
-                if (a[i] >= maksverdi)       // denne blir sann til slutt
+        for (int i = 1; ; i++)         // i starter med 1
+            if (a[i] >= maksverdi)       // denne blir sann til slutt
+            {
+                if (i == sist)             // sjekker om vi er ferdige
                 {
-                    if (i == sist)             // sjekker om vi er ferdige
-                    {
-                        a[sist] = temp;          // legger siste verdi tilbake
-                        return a[sist] >= maksverdi ? sist : m; // er siste størst?
-                    }
-                    else
-                    {
-                        maksverdi = a[i];        // maksverdi oppdateres
-                        m = i;                   // m oppdateres
-                    }
+                    a[sist] = temp;          // legger siste verdi tilbake
+                    return a[sist] >= maksverdi ? sist : m; // er siste størst?
+                } else {
+                    maksverdi = a[i];        // maksverdi oppdateres
+                    m = i;                   // m oppdateres
                 }
-        } // maks
-
-        public static void main(String[] args)
-        {
-            int n = 100_000;        // tabellstørrelse
-            int antall = 2_000;     // antall gjentagelser
-
-            long tid = 0;           // for tidsmåling
-
-            int a[] = randPerm(n);  // en permutasjon av 1, . .  n
-
-            tid = System.currentTimeMillis();  // leser av klokken
-            for (int i = 0; i < antall; i++) kostnader(a);
-            tid = System.currentTimeMillis() - tid;  // medgått tid
-            System.out.println("Faste kostnader: " + tid + " millisek");
-
-            tid = System.currentTimeMillis();  // leser av klokken
-            for (int i = 0; i < antall; i++) maks1(a);
-            tid = System.currentTimeMillis() - tid;  // medgått tid
-            System.out.println("Maks1-metoden: " + tid + " millisek");
-
-            tid = System.currentTimeMillis();  // leser av klokken
-            for (int i = 0; i < antall; i++) maks2(a);
-            tid = System.currentTimeMillis() - tid;  // medgått tid
-            System.out.println("Maks2-metoden: " + tid + " millisek");
-
-            tid = System.currentTimeMillis();  // leser av klokken
-            for (int i = 0; i < antall; i++) maks3(a);
-            tid = System.currentTimeMillis() - tid;  // medgått tid
-            System.out.println("Maks3-metoden: " + tid + " millisek");
+            }
+    } // maks
 
 
-        }
-    } // class Program
+} // class Program
 
--- src/forelesningRekursivtBinarySearch/Tabell.java
+++ src/forelesningRekursivtBinarySearch/Tabell.java
@@ -1,12 +1,14 @@
 package forelesningRekursivtBinarySearch;
 
+
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.Random;
 
 public class Tabell {
 
     public static void main(String[] args) {
-       int[] a = {1,2,31,123,1,12,1,212,1};
+        int[] a = {1, 2, 31, 123, 1, 12, 1, 212, 1};
         /*System.out.println(min(a));
         System.out.println(maks(a));
         System.out.println(maks(a,2,5));
@@ -14,8 +16,12 @@ public class Tabell {
         Tabell tabell = new Tabell();
 
          */
+        System.out.println(Arrays.toString(a));
+        sortering(a);
+        System.out.println(Arrays.toString(a));
 
-        System.out.println(Arrays.binarySearch(a,0,a.length,1));
+        //System.out.println(Arrays.toString(nestMaksBakerst(a)));
+        //System.out.println(maks(a));
 
 
 
@@ -38,7 +44,6 @@ public class Tabell {
     }
 
 
-
     public static void fratilKontroll(int tablengde, int fra, int til) {
         if (fra < 0)                             // fra er negativ
             throw new ArrayIndexOutOfBoundsException
@@ -53,8 +58,7 @@ public class Tabell {
                     ("fra(" + fra + ") > til(" + til + ") - illegalt intervall!");
     }
 
-    public static void vhKontroll(int tablengde, int v, int h)
-    {
+    public static void vhKontroll(int tablengde, int v, int h) {
         if (v < 0)
             throw new ArrayIndexOutOfBoundsException("v(" + v + ") < 0");
 
@@ -81,12 +85,8 @@ public class Tabell {
 
     public static void skriv(int[] a, int fra, int til) {
         fratilKontroll(a.length, fra, til);
-        if (til - fra > 0) {
-            System.out.print(a[fra]);
-            for (int i = fra; i < til; ++i) {
-                System.out.print(" " + a[i]);
-            }
-        }
+
+        for (int i = fra; i < til; i++) System.out.print(a[i] + " ");
     }
 
 
@@ -95,7 +95,9 @@ public class Tabell {
     }
 
     public static void skrivln(int[] a, int fra, int til) {
-        skriv(a, fra, til);
+        fratilKontroll(a.length, fra, til);
+
+        for (int i = fra; i < til; i++) System.out.print(a[i] + " ");
         System.out.println();
     }
 
@@ -103,6 +105,19 @@ public class Tabell {
     public static void skrivln(int[] a) {
         skrivln(a, 0, a.length);
 
+    }
+
+    public static void skriv(char[] c, int fra, int til) {
+        fratilKontroll(c.length, fra, til);
+
+        for (int i = fra; i < til; i++) System.out.print(c[i] + " ");
+    }
+
+    public static void skrivln(char[] c, int fra, int til) {
+        fratilKontroll(c.length, fra, til);
+
+        for (int i = fra; i < til; i++) System.out.print(c[i] + " ");
+        System.out.println();
     }
 
     public static int[] randPerm(int n)  // en versjon som ikke virker
@@ -144,6 +159,30 @@ public class Tabell {
         return m;  // posisjonen til største verdi i a[fra:til>
     }
 
+    public static int maksOppgave3(int[] a, int fra, int til) {
+        if (a == null) {
+            throw new NullPointerException("Parametertabellen er null!");
+        }
+
+        fratilKontroll(a.length, fra, til);
+
+        if (fra == til) {
+            throw new NoSuchElementException("fra(" + fra + ") = til " + til + " ) - tomt tabellintervall!");
+
+        }
+        int m = fra;
+        int maksverdi = a[fra];
+
+        for (int i = fra + 1; i < til; ++i) {
+            if (a[i] > maksverdi) {
+                m = i;
+                maksverdi = a[m];
+            }
+
+        }
+        return m;
+    }
+
 
     public static int maks(int[] a)  // bruker hele tabellen
     {
@@ -166,6 +205,79 @@ public class Tabell {
         }
 
         return m;  // posisjonen til største verdi i a[fra:til>
+    }
+
+    public static int[] nestMaks(int[] a)  // legges i class Tabell
+    {
+        int n = a.length;   // tabellens lengde
+
+        if (n < 2) throw   // må ha minst to verdier!
+                new java.util.NoSuchElementException("a.length(" + n + ") < 2!");
+
+        int m = maks(a);  // m er posisjonen til tabellens største verdi
+
+        int nm;           // nm skal inneholde posisjonen til nest største verdi
+
+        if (m == 0)                            // den største ligger først
+        {
+            nm = maks(a, 1, n);                  // leter i a[1:n>
+        } else if (m == n - 1)                   // den største ligger bakerst
+        {
+            nm = maks(a, 0, n - 1);              // leter i a[0:n-1>
+        } else {
+            int mv = maks(a, 0, m);              // leter i a[0:m>
+            int mh = maks(a, m + 1, n);          // leter i a[m+1:n>
+            nm = a[mh] > a[mv] ? mh : mv;        // hvem er størst?
+        }
+
+        return new int[]{m, nm};      // m i posisjon 0 , nm i posisjon 1
+
+    } // nestMaks
+
+    public static int[] nestMaksForst(int[] a) {
+        if (a.length < 2) // må ha minst to verdier!
+            throw new IllegalArgumentException("a.length(" + a.length + ") < 2!");
+
+        int m = Tabell.maks(a);  // m er posisjonen til tabellens største verdi
+
+        Tabell.bytt(a, 0, m);  // bytter om slik at den største kommer forrest
+
+        int k = Tabell.maks(a, 1, a.length);
+
+        if (k == m) k = 0; // den nest største lå opprinnelig forrest
+
+        Tabell.bytt(a, 0, m); // bytter tilbake
+
+        return new int[]{m, k};
+
+    }
+
+    public static int[] nestMaksBakerst(int[] a)
+    {
+        if (a.length < 2) // må ha minst to verdier!
+            throw new IllegalArgumentException("a.length(" + a.length + ") < 2!");
+
+        int m = Tabell.maks(a);  // m er posisjonen til tabellens største verdi
+
+        Tabell.bytt(a,a.length-1,m);  // bytter om slik at den største kommer bakerst
+
+        int k = Tabell.maks(a,0,a.length-1);
+
+        if (k == m) k = a.length - 1; // den nest største lå opprinnelig bakerst
+
+        Tabell.bytt(a,a.length-1,m); // bytter tilbake
+
+        return new int[] {m,k};
+
+    } // nestMaks
+
+    public static void sortering(int[] a)
+    {
+        for (int i = a.length; i > 1; i--)
+        {
+            int m = Tabell.maks(a,0,i);
+            Tabell.bytt(a,i-1,m);
+        }
     }
 
 
